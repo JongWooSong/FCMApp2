8af1129ecdd6753888d56b66120a7fef5fb8f4c9
JeonJiYun <wldbs281@naver.com>
2017-07-26 14:20:03 +0900

    hi

--- app/src/main/java/com/swu/fcm/app/MainActivity.java
+++ app/src/main/java/com/swu/fcm/app/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         //토큰 저장 로직 - 송강가사 오늘 피곤하다. 잘하자!
         //송강사 아아아아아!
         // 아아아아
+        // 배고파 너무 고파
         String savedToken = PrefUtil.getPref(this, MyFirebaseInstanceIDService.PUSH_TOKEN);
         if(savedToken != null || savedToken.length() == 0) {
             savedToken = FirebaseInstanceId.getInstance().getToken();
